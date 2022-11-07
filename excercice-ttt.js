a26e461b91172cda9d04cb4c24c1505869e72bb3
Aurore-444 <117636619+Aurore-444@users.noreply.github.com>
2022-11-07 14:45:34 +0100

    initial commit

---
+++ excercice-ttt.js
@@ -0,0 +1,10 @@
+const etatJeu = {
+    cellules: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ],
+    // Deux joueurs possibles : "BLEU" ou "ORANGE"
+    joueur: 'BLEU',
+  };
+  
